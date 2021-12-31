7cc534687bc90b79e1da99d70bdb9ee1c08cbca3
DAVID Axel <axel.david@etu.univ-amu.fr>
2021-12-31 18:52:36 +0100

    Add ttl_new

---
+++ TTL_new/choix.md
@@ -0,0 +1,5 @@
+Le choix de faire une installation manuelle de CodeIgniter permet de mettre à jour quand on veut et ainsi faire les fusions des mises à jour par nous même.
+Pour l'installation du site, il suffit de copier le dossier la structure codeigniter
+Puis de paramétrer le fichier .env, avec la configuration du site et des fichiers de configurations
+
+L'installation avec l'outil 'composer' peut modifier les fichiers de configuration sans prévenir l'adminsitrateur, alors que la configuration manuelle, force l'administrateur à faire l'écrasement du dossier system par l'administrateur pour mettre à jour
---
+++ TTL_new/public/accueil.php
@@ -0,0 +1,31 @@
+<?php
+?>
+
+<!DOCTYPE html>
+    <html lang="fr">
+        <head>
+            <title>Accueil - TrouveTonLogement</title>
+            <meta charset="utf-8">
+        </head>
+
+        <body>
+            <nav style="background-color: #002b36">
+                !-- La barre de navigation
+                !-- Lien vers l'accueil du site
+                !-- Lien vers toutes les annonces
+                !-- Lien pour se connecter
+            </nav>
+
+            <section>
+
+            </section>
+
+            <footer style="background-color: #1b1b1b">
+                <ul>
+                    <li><a href="a_propos_nous.html">A propos de Nous</a></li>
+                    <li><a href="cgu.html">Conditions Générale d'Utilisation</a></li>
+                    <li><a href="cgv.html">Conditions Générale</a> </li>
+                </ul>
+            </footer>
+        </body>
+    </html>
