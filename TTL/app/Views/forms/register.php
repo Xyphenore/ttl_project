f5407497af3be64889c92dd94233382bc69d5691
COET Muriel <muriel.coet@etu.univ-amu.fr>
2022-01-04 22:22:45 +0100

    modifié :         TTL/app/Views/forms/register.php

--- TTL/app/Views/forms/register.php
+++ TTL/app/Views/forms/register.php
@@ -9,36 +9,36 @@
 
                 <div class='row g-3'>
                     <div class="form-group pb-2 col-lg-6">
-                        <label for='email' class="form-label">Adresse email</label>
+                        <label for='email' class="form-label">Adresse email*</label>
                         <input type='email' name='email' class="form-control" id="email" placeholder="Entrer votre adresse email"/>
                     </div>
 
                     <div class='form-group pb-2 col-lg-6'>
-                        <label for='pseudo' class='form-label'>Pseudo</label>
+                        <label for='pseudo' class='form-label'>Pseudo*</label>
                         <input type='text' name='pseudo' id='pseudo' class='form-control' placeholder='Entrer votre pseudo'/>
                     </div>
                 </div>
 
                 <div class='row g-3'>
                     <div class='form-group pb-2 col-lg-6'>
-                        <label for='pass' class='form-label'>Mot de passe</label>
+                        <label for='pass' class='form-label'>Mot de passe*</label>
                         <input type='password' name='pass' id="pass" class="form-control" placeholder="Entrer le mot de passe"/>
                     </div>
 
                     <div class='form-group pb-2 col-lg-6'>
-                        <label for='confirm' class='form-label'>Confirmation du mot de passe</label>
+                        <label for='confirm' class='form-label'>Confirmation du mot de passe*</label>
                         <input type='password' name='confirm' id='confirm' class='form-control' placeholder='Confirmer le mot de passe'/>
                     </div>
                 </div>
 
                 <div class='row g-3'>
                     <div class='form-group pb-2 col-lg-6'>
-                        <label for='nom' class='form-label'>Nom</label>
+                        <label for='nom' class='form-label'>Nom*</label>
                         <input type='text' name='nom' id='nom' class='form-control' placeholder='Entrer votre nom'/>
                     </div>
 
                     <div class='form-group pb-2 col-lg-6'>
-                        <label for='prenom' class='form-label'>Prénom</label>
+                        <label for='prenom' class='form-label'>Prénom*</label>
                         <input type='text' name='prenom' id='prenom' class='form-control' placeholder='Entrer votre prénom'/>
                     </div>
                 </div>
@@ -49,7 +49,7 @@
                     S'inscrire
                 </button>
 
-                <a href="<?= esc(base_url('connexion')) ?>" role='button' class='btn btn-secondary mx-sm-5'>
+                <a href="<?= esc(base_url('login')) ?>" role='button' class='btn btn-secondary mx-sm-5'>
                     Connexion
                 </a>
             </div>
